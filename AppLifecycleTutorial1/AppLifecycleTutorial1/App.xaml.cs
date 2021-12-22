27d490b973de3c781e698e04696b5efb6c0d857c
isa20 <isa20@DESKTOP-22PN69P>
2021-12-22 14:19:53 +0300

    Добавьте файлы проекта.

---
+++ AppLifecycleTutorial1/AppLifecycleTutorial1/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppLifecycleTutorial1
+{
+    public partial class App : Application
+    {
+        const string displayText = "displayText";
+
+        public string DisplayText { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            Console.WriteLine("OnStart");
+
+            if (Properties.ContainsKey(displayText))
+            {
+                DisplayText = (string)Properties[displayText];
+            }
+        }
+
+        protected override void OnSleep()
+        {
+            Console.WriteLine("OnSleep");
+            Properties[displayText] = DisplayText;
+        }
+
+        protected override void OnResume()
+        {
+            Console.WriteLine("OnResume");
+        }
+    }
+}
